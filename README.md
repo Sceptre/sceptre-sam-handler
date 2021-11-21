09b7a8299cc56dc1abb7d138fd7586230191141c
Jon Falkenstein <jfalkenstein@trueaccord.com>
2021-11-20 20:12:55 -0600

    creating the sam handler

--- README.md
+++ README.md
@@ -1,11 +1,81 @@
 # README
 
-Add your handler readme here. Remember to include the following:
+## What is this?
+`sceptre-sam-handler` is a TemplateHandler for Sceptre (versions 2.7 and up) that lets you use an 
+AWS SAM template (and its associated project) as a stack's template. 
 
-- Tell people how to install it (e.g. pip install ...).
-- Be clear about the purpose of the template handler, its capabilities and limitations.
-- Tell people how to use it.
-- Give examples of the template handler in use.
+This template handler will run `sam build` and then `sam package` from the indicated SAM Template's
+directory in order to generate a CloudFormation-ready template.
 
-If you have any questions or encounter an issue
-[please open an issue](https://github.com/Sceptre/project/issues/new)
+## How to install sceptre-sam-handler
+
+Simply `pip install scepre-sam-handler`!
+
+## How to use sceptre-sam-handler
+
+The template "type" for this handler is `sam`. 
+
+This handler takes several arguments, two of which are required.
+
+### Arguments:
+* `sam_template_path` (string, required): The path **from the current working directory** (NOT the 
+* project path) to the SAM Template.
+* `artifact_bucket_name` (string, required): The bucket name where artifacts should be uploaded to 
+on S3 during the packaging process. If your project has a `template_bucket_name`, you can set this
+to `{{ template_bucket_name }}`.
+* `artifact_prefix` (string, optional): The prefix to apply to artifacts uploaded to S3. This can be 
+the project's `{{ template_key_prefix }}`.
+* `sam_build_args` (dict, optional): Additional key/value pairs to supply to `sam build`. For 
+flag-type arguments that have no value, set the value to "True".
+* `sam_package_args` (dict, optional): Additional key/value pairs to apply to `sam package`. The
+same is true here as for `sam_build_args` for flag-type arguments.
+
+### Default behavior
+SAM commands are invoked using the system shell in a subprocess, with stdout redirected to stderr.
+Artifacts will be uploaded using the `artifact_bucket_name` and `artifact_prefix` arguments, the 
+`project_code`, and the Sceptre stack name.
+
+For example, given an `artifact_bucket_name` of "bucket", `artifact_prefix` of "prefix", a 
+`project_code` of "project" and a stack config located at "config/indigo/sam-application.yaml", SAM 
+artifacts will be uploaded to:
+
+`s3://bucket/prefix/project/indigo/sam-application/sam_artifacts/`
+
+By default, these will be the sam commands that are run _from the template's directory_:
+```shell
+sam build --cached --template-file [sam_template_path as absolute path]
+sam package \
+  --s3-bucket [artifact_bucket_name argument] \
+  --region [the stack region] \
+  --s3-prefix [the prefix described above] \
+  --template-file [sam_template_path as absolute path]
+```
+
+If any additional arguments are desired for to be passed to SAM, you can specify those with dicts for
+the `sam_build_args` and `sam_package_args` template handler arguments. These key/value pairs will
+override the defaults. For any flag-type arguments, set the value to `True`. If you want to remove
+a default argument (such as the `--cached` flag for `sam build`), set the value to `None`.
+
+### IAM and authentication
+
+This handler uses the stack's connection information to generate AWS environment variables and sets
+those on the sam process, ensuring that the AWS authentication configuration on the stack config and
+project is carried over to SAM without any need for additional arguments.
+
+**Important:** SAM creates CloudFormation-ready templates via `sam package`, which uploads built 
+artifacts to S3 in the process. This means that Sceptre commands that do not normally require S3
+actions (such as `generate`, `validate`, `diff`, and others) will require them when using this 
+handler. You will need to ensure that any user or role executing these commands has "s3:PutObject"
+permissions.
+
+### Example
+```yaml
+template:
+    type: sam
+    sam_template_path: path/from/my/cwd/template.yaml
+    artifact_bucket_name: {{ template_bucket_name }}
+    artifact_prefix: {{ template_key_prefix }}
+    sam_build_args: 
+        use-container: True
+```
+
--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,9 +1,235 @@
-# -*- coding: utf-8 -*-
+import subprocess
+import sys
+from pathlib import Path
+from unittest import TestCase
+from unittest.mock import Mock, create_autospec
+
+from botocore.credentials import Credentials
+from pyfakefs.fake_filesystem_unittest import TestCase as FsTestCase
+from sceptre.connection_manager import ConnectionManager
+
+from sam_handler.handler import SAM, SamInvoker
 
 
-class TestStackActions(object):
-    def setup_method(self, test_method):
-        pass
+class TestSAM(FsTestCase):
+    def setUp(self):
+        super().setUp()
+        self.setUpPyfakefs()
+        self.template_contents = 'hello!'
+        self.processed_contents = 'goodbye!'
+        self.fs.create_file('my/random/path.yaml', contents=self.template_contents)
+        self.arguments = {
+            'sam_template_path': 'my/random/path.yaml',
+            'artifact_prefix': 'prefix',
+            'artifact_bucket_name': 'bucket'
+        }
+        self.invoker = Mock(**{
+            'spec': SamInvoker,
+            'invoke.side_effect': self.fake_invoke
+        })
+        self.invoker_class = create_autospec(SamInvoker, return_value=self.invoker)
+        self.temp_dir = '/temp'
+        self.get_temp_dir = lambda: self.temp_dir
 
-    def test_dummy_test(self):
-        pass
+        self.region = 'region'
+
+        self.connection_manager = Mock(**{
+            'spec': ConnectionManager,
+            'region': self.region,
+        })
+        self.name = 'top/mid/stack'
+        self.handler = SAM(
+            self.name,
+            connection_manager=self.connection_manager,
+            arguments=self.arguments,
+            invoker_class=self.invoker_class,
+            get_temp_dir=self.get_temp_dir
+        )
+        self._is_built = False
+
+    def fake_invoke(self, command, args):
+        if command == 'build':
+            self._is_built = True
+        elif command == 'package':
+            self.assertTrue(self._is_built)
+            output_file = Path(args['output-template-file'])
+            output_file.write_text(self.processed_contents)
+
+    def test_handle__instantiates_invoker_with_correct_args(self):
+        self.handler.handle()
+        self.invoker_class.assert_called_with(
+            self.connection_manager,
+            Path(self.arguments['sam_template_path']).parent.absolute()
+        )
+
+    def test_handle__invokes_build_with_default_arguments(self):
+        self.handler.handle()
+        self.invoker.invoke.assert_any_call(
+            'build',
+            {
+                'cached': True,
+                'template-file': str(Path(self.arguments['sam_template_path']).absolute())
+            }
+        )
+
+    def test_handle__sam_build_args_specified__invokes_build_with_all_build_args(self):
+        self.arguments['sam_build_args'] = {'use-container': True}
+        self.handler.handle()
+        self.invoker.invoke.assert_any_call(
+            'build',
+            {
+                'cached': True,
+                'template-file': str(Path(self.arguments['sam_template_path']).absolute()),
+                'use-container': True
+            }
+        )
+
+    def test_handle__invokes_package_with_default_arguments(self):
+        self.handler.handle()
+        expected_temp_dir = Path(self.temp_dir) / (self.name + '.yaml')
+        expected_prefix = '/'.join([
+            self.arguments['artifact_prefix'],
+            *self.name.split('/'),
+            'sam_artifacts'
+        ])
+
+        self.invoker.invoke.assert_any_call(
+            'package',
+            {
+                's3-bucket': self.arguments['artifact_bucket_name'],
+                'region': self.region,
+                's3-prefix': expected_prefix,
+                'output-template-file': expected_temp_dir,
+                'template-file': str(Path(self.arguments['sam_template_path']).absolute())
+            }
+        )
+
+    def test_handle__sam_package_args_specified__invokes_package_with_default_arguments(self):
+        self.arguments['sam_package_args'] = {'new': 'arg'}
+        self.handler.handle()
+        expected_temp_dir = Path(self.temp_dir) / (self.name + '.yaml')
+        expected_prefix = '/'.join(
+            [
+                self.arguments['artifact_prefix'],
+                *self.name.split('/'),
+                'sam_artifacts'
+            ]
+        )
+
+        self.invoker.invoke.assert_any_call(
+            'package',
+            {
+                's3-bucket': self.arguments['artifact_bucket_name'],
+                'region': self.region,
+                's3-prefix': expected_prefix,
+                'output-template-file': expected_temp_dir,
+                'template-file': str(Path(self.arguments['sam_template_path']).absolute()),
+                'new': 'arg'
+            }
+        )
+
+    def test_handle__returns_contents_of_destination_template_file(self):
+        result = self.handler.handle()
+        self.assertEqual(self.processed_contents, result)
+
+
+class TestSamInvoker(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.credentials = Mock(
+            spec=Credentials,
+            access_key='access',
+            secret_key='secret',
+            token='token'
+        )
+
+        self.profile = 'professor'
+        self.region = 'down under'
+        self.iam_role = 'iam not!'
+
+        self.connection_manager = Mock(**{
+            'spec': ConnectionManager,
+            '_get_session.return_value.get_credentials.return_value': self.credentials,
+            'profile': self.profile,
+            'region': self.region,
+            'iam_role': self.iam_role
+
+        })
+        self.sam_directory = Path('/path/to/my/sam/directory')
+        self.envs = {
+            'some': 'env'
+        }
+        self.run_subprocess = Mock(spec=subprocess.run)
+
+        self.invoker = SamInvoker(
+            connection_manager=self.connection_manager,
+            sam_directory=self.sam_directory,
+            environment_variables=self.envs,
+            run_subprocess=self.run_subprocess
+        )
+
+    def assert_sam_command(self, command, *, expected_envs=None):
+        if expected_envs:
+            envs = expected_envs
+        else:
+            envs = self.envs.copy()
+            envs.update(
+                AWS_ACCESS_KEY_ID=self.credentials.access_key,
+                AWS_SECRET_ACCESS_KEY=self.credentials.secret_key
+            )
+            if self.credentials.token is not None:
+                envs['AWS_SESSION_TOKEN'] = self.credentials.token
+
+        self.run_subprocess.assert_called_with(
+            command,
+            shell=True,
+            check=True,
+            cwd=self.sam_directory,
+            stdout=sys.stderr,
+            env=envs
+        )
+
+    def test_invoke__runs_sam_command_with_args(self):
+        args = {
+            'key': 'value',
+            'flag': True,
+            'ignore me': None,
+        }
+        self.invoker.invoke('build', args)
+        expected_command = 'sam build --key "value" --flag'
+        self.assert_sam_command(expected_command)
+
+    def test_invoke__runs_sam_command_with_empty_args(self):
+        self.invoker.invoke('build', {})
+        expected_command = 'sam build'
+        self.assert_sam_command(expected_command)
+
+    def test_sam_command__injects_aws_environment_variables_into_sam_command(self):
+        self.invoker.invoke('command', {})
+
+        self.assert_sam_command(
+            'sam command',
+            expected_envs={
+                **self.envs,
+                **{
+                    'AWS_ACCESS_KEY_ID': self.credentials.access_key,
+                    'AWS_SECRET_ACCESS_KEY': self.credentials.secret_key,
+                    'AWS_SESSION_TOKEN': self.credentials.token,
+                }
+            }
+        )
+
+    def test_sam_command__session_token_is_none__does_not_inject_that_to_envs(self):
+        self.credentials.token = None
+        self.invoker.invoke('command', {})
+
+        self.assert_sam_command(
+            'sam command',
+            expected_envs={
+                **self.envs,
+                **{
+                    'AWS_ACCESS_KEY_ID': self.credentials.access_key,
+                    'AWS_SECRET_ACCESS_KEY': self.credentials.secret_key,
+                }
+            }
+        )
